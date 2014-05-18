b07a160be9404198895bce45594765a52b97f107
techhead55 <techhead55@gmail.com>
2014-05-18 14:10:31 +1000

    Implemented file serving for interface and created blank html interface.

---
+++ Server/app.js
@@ -0,0 +1,21 @@
+console.log("SSI server starting.")
+var express = require('express'),
+	app = express(),
+    server = require('http').createServer(app),
+    io = require("socket.io").listen(server);
+server.listen(3780);
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/interface/index.html');
+});
+app.get('/javascript', function(req, res){
+	res.sendfile(__dirname + '/interface/main.js');
+});
+app.get('/stylesheet', function(req, res){
+	res.sendfile(__dirname + '/interface/stylesheet.css');
+});
+io.sockets.on('connection', function(socket){
+    console.log("Interface connected")
+    socket.on('example', function(data){
+        console.log(data);
+    });
+});
