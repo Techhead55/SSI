08bf4ae5b55fe43300149fa28da4c7f410be52fd
techhead55 <techhead55@gmail.com>
2014-05-19 21:26:05 +1000

    Implemented the display of the database in the server side interface.

--- Server/interface/main.js
+++ Server/interface/main.js
@@ -1,8 +1,29 @@
-var socket = io.connect('http://localhost:3780'),
-    data = {};
-socket.on('connect', function(){
-    socket.emit('requestdata');
-    socket.on('data', function(value){
-        data = value;
+window.onload = function(){
+    var socket = io.connect('http://localhost:3780'),
+        data = {},
+        databaseElement = document.getElementById("database");
+    function addEle (contents, level){
+        var element = document.createElement("div");
+        element.style.paddingLeft = (((level)*30)+30)+"px";
+        element.innerHTML = contents;
+        element.setAttribute("class", "databaseElement");
+        databaseElement.appendChild(element);
+    }
+    function process(object, level){
+        if (object instanceof Object){
+            for (item in object) {
+                addEle(item+":", level);
+                process(object[item], level+1);
+            }
+        } else {
+            addEle(object, level);
+        }
+    }
+    socket.on('connect', function(){
+        socket.emit('requestdata');
+        socket.on('data', function(value){
+            data = value;
+            process(data, 0);
+        });
     });
-});
+};
